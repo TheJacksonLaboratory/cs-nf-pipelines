75402a42de37574d4b3ef2f4aa93c19f21017c5c
guglib <barry.guglielmo@jax.org>
2022-03-02 10:23:39 -0500

    ribo_intervals null rnaseq mouse works

--- run.sh
+++ run.sh
@@ -13,4 +13,4 @@ cd $SLURM_SUBMIT_DIR
 # LOAD SINGULARITY
 ml singularity
 # RUN TEST PIPELINE
-~/nextflow main.nf --workflow rnaseq --gen_org mouse --comment "fixing rnaseq mouse" --ribo_intervals=null
+~/nextflow main.nf --workflow rnaseq --gen_org mouse --comment "fixing rnaseq mouse"
