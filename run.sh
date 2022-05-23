eb36d188bd5cd4f855faeef75a265d93109d09b5
Mike Lloyd <mike.lloyd@jax.org>
2022-05-23 11:56:44 -0400

    updated run script to point at nextflow module

--- run.sh
+++ run.sh
@@ -3,14 +3,21 @@
 #SBATCH --job-name=CS_nextflow_example
 #SBATCH -p compute
 #SBATCH -q batch
-#SBATCH -t 20:00:00
-#SBATCH --mem=16G
+#SBATCH -t 00:01:00
+#SBATCH --mem=2G
 #SBATCH --ntasks=1
 
 cd $SLURM_SUBMIT_DIR
 
-# LOAD SINGULARITY
-module load singularity
+# LOAD NEXTFLOW
+module use --append /projects/omics_share/meta/modules
+module load nextflow
 
 # RUN TEST PIPELINE
-~/nextflow main.nf -profile sumner --workflow rnaseq --gen_org mouse --sample_folder 'test/rna/mouse' --pubdir '/fastscratch/outputDir' -w '/fastscratch/outputDir/work'
+nextflow main.nf \
+-profile sumner \
+--workflow rnaseq \
+--gen_org mouse \
+--sample_folder 'test/rna/mouse' \
+--pubdir '/fastscratch/outputDir' \
+-w '/fastscratch/outputDir/work'
