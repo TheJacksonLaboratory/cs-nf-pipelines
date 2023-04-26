f1785f4706f9775ef562a07cdc3d548f7df87810
Mike Lloyd <Mike.Lloyd@jax.org>
2023-04-25 20:44:27 -0400

    run scripts updated

---
+++ run_scripts/rnafusion_human.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --mail-user=first.last@jax.org
+#SBATCH --job-name=rna_fusion_human
+#SBATCH --mail-type=END,FAIL
+#SBATCH -p compute
+#SBATCH -q batch
+#SBATCH -t 72:00:00
+#SBATCH --mem=1G
+#SBATCH --ntasks=1
+
+cd $SLURM_SUBMIT_DIR
+
+# LOAD NEXTFLOW
+module use --append /projects/omics_share/meta/modules
+module load nextflow
+
+# RUN PIPELINE
+nextflow ../main.nf \
+--workflow rna_fusion \
+-profile sumner \
+--sample_folder <PATH_TO_YOUR_SEQUENCES> \
+--gen_org human \
+--pubdir "/fastscratch/${USER}/outputDir" \
+-w "/fastscratch/${USER}/outputDir/work" \
+--comment "This script will run rna_fusion on human samples using default hg38"
